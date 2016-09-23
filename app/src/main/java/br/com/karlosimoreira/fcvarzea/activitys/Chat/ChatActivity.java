9dafa2c4f1ecf2fd9f3c728e8f4c56257d8f8f16
Carlos Alberto Reis Moreira <karlosimoreira@yahoo.com.br>
2016-09-23 14:29:39 -0300

    fundo e checbox

---
+++ app/src/main/java/br/com/karlosimoreira/fcvarzea/activitys/Chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package br.com.karlosimoreira.fcvarzea.activitys.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.karlosimoreira.fcvarzea.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
